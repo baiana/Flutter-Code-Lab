d5c4c1437e52fca699f8bc0850bbfabaf112b034
ana Dias <ana.luisa@cubos.io>
2020-04-01 18:32:51 -0300

    feat: add mantras package

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:mantras/mantras.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    String mantra = Mantras().getMantra();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Aqui está a app bar',),
+          title: const Text(
+            'Aqui está a app bar',
+          ),
           backgroundColor: Colors.amber,
         ),
-        body: const Center(
-          child: const Text('olá! galeura'),
+        body: Center(
+          child: Text(mantra),
         ),
       ),
     );
