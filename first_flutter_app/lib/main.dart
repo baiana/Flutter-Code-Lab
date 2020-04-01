37b86092f40baddd4675ab0391672984152c4900
ana Dias <analuisa104@gmail.com>
2020-04-01 20:11:46 -0300

    feat: add list of mantras with infinity scroll

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:mantras/mantras.dart';
 
@@ -9,35 +11,59 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
+        backgroundColor: Color.fromARGB(210, 208, 254, 245),
         appBar: AppBar(
           title: const Text(
-            'Olha essa mensagem motivacional',
+            'Mantras do dia',
           ),
-          backgroundColor: Colors.amber,
+          backgroundColor: Color.fromARGB(255, 125, 206, 210),
         ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(padding: EdgeInsets.only(top: 20, left: 20, right: 20)),
-              Center(child: RandomMantras())
-            ]),
+        body: RandomMantras(),
       ),
     );
   }
 }
 
 class RandomMantraState extends State<RandomMantras> {
+  final List<String> _suggestions = <String>[];
+
   @override
   Widget build(BuildContext context) {
-    final String mantraRandom = Mantras().getMantra();
-    return Text(
+    return _buildSuggestions();
+    //final String mantraRandom = Mantras().getMantra();
+    /*return Text(
       mantraRandom,
-      style: Theme.of(context)
-          .textTheme
-          .title
-          .merge(TextStyle(color: Colors.black)),
+      style: Theme.of(context).textTheme.body2.merge(
+          TextStyle(color: Color.fromARGB(255, 8, 20, 29), fontSize: 26)),
       textAlign: TextAlign.center,
+    );*/
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.only(left: 16,top: 16,bottom: 16,right: 56),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          final int index = i ~/ 2;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(RandomMantras().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(String pair) {
+    return ListTile(
+      title: Text(
+        pair,
+        style: Theme.of(context).textTheme.body1.merge(
+            TextStyle(color: Color.fromARGB(255, 8, 20, 29), fontSize: 16)),
+        textAlign: TextAlign.start,
+      ),
     );
   }
 }
@@ -45,6 +71,12 @@ class RandomMantraState extends State<RandomMantras> {
 class RandomMantras extends StatefulWidget {
   @override
   RandomMantraState createState() => RandomMantraState();
+
+  List<String> take(int number) {
+    var list = List<String>();
+    for (var i = 0; i < number; i++) {
+      list.add(Mantras().getMantra());
+    }
+    return list;
+  }
 }
-
-
