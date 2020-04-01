845ec9ce964cd6dc80d98ec66782b8a383746610
ana Dias <analuisa104@gmail.com>
2020-04-01 20:27:00 -0300

    feat: add click response to favorite icon

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -26,29 +26,21 @@ class MyApp extends StatelessWidget {
 
 class RandomMantraState extends State<RandomMantras> {
   final List<String> _suggestions = <String>[];
+  final Set<String> _saved = Set<String>();
 
   @override
   Widget build(BuildContext context) {
     return _buildSuggestions();
-    //final String mantraRandom = Mantras().getMantra();
-    /*return Text(
-      mantraRandom,
-      style: Theme.of(context).textTheme.body2.merge(
-          TextStyle(color: Color.fromARGB(255, 8, 20, 29), fontSize: 26)),
-      textAlign: TextAlign.center,
-    );*/
   }
 
   Widget _buildSuggestions() {
     return ListView.builder(
-        padding: const EdgeInsets.only(left: 16,top: 16,bottom: 16,right: 56),
+        padding: const EdgeInsets.all(10),
         itemBuilder: (BuildContext _context, int i) {
           if (i.isOdd) {
             return Divider();
           }
-
           final int index = i ~/ 2;
-
           if (index >= _suggestions.length) {
             _suggestions.addAll(RandomMantras().take(10));
           }
@@ -57,6 +49,7 @@ class RandomMantraState extends State<RandomMantras> {
   }
 
   Widget _buildRow(String pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair,
@@ -64,6 +57,19 @@ class RandomMantraState extends State<RandomMantras> {
             TextStyle(color: Color.fromARGB(255, 8, 20, 29), fontSize: 16)),
         textAlign: TextAlign.start,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.pinkAccent : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
