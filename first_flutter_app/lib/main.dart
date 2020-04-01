147de2d7f52c251277677055939729395bf11400
ana Dias <ana.luisa@cubos.io>
2020-04-01 19:11:15 -0300

    feat: create column and add children

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -4,23 +4,45 @@ import 'package:mantras/mantras.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    String mantra = Mantras().getMantra();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text(
-            'Aqui está a app bar',
+            'Olha essa mensagem motivacional',
           ),
           backgroundColor: Colors.amber,
         ),
-        body: Center(
-          child: Text(mantra),
-        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(padding: EdgeInsets.only(top: 20, left: 20, right: 20)),
+              Center(child: RandomMantras())
+            ]),
       ),
     );
   }
+}
+
+class RandomMantraState extends State<RandomMantras> {
+  @override
+  Widget build(BuildContext context) {
+    final String mantraRandom = Mantras().getMantra();
+    return Text(
+      mantraRandom,
+      style: Theme.of(context)
+          .textTheme
+          .title
+          .merge(TextStyle(color: Colors.black)),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class RandomMantras extends StatefulWidget {
+  @override
+  RandomMantraState createState() => RandomMantraState();
 }
